ed3f333c271f2285d11c8c8be76d8f71873c3d61
xliu43 <36863407+xliu43@users.noreply.github.com>
2018-03-25 19:54:50 -0600

    revised A10 switch library

--- embeded system/C libraries/Switches_C.h
+++ embeded system/C libraries/Switches_C.h
@@ -12,3 +12,34 @@ void Wait_For_UpSwitch_Up(void) ;
 void Wait_For_DownSwitch_Up(void);
 
 void Wait_For_Switch_Up(unsigned char sw);
+
+
+/*
+   sw=Get_Switches();
+                switch(sw){
+                
+                                                        case 0b00010000: //upswitch
+                                                        VoltageMax=0;
+                                                      
+                                                        break;
+                                                        
+                                                        case 0b00000100: //downswitch 
+                                                        VoltageMin=5.115;
+                                                     
+                                                        break; 
+                                                        
+                                                        case 0b00001000 :  //left switch 
+                                                         break;
+                                                         
+                                                        case 0b0000010 :  //right switch 
+                                                         break;    
+                                                         
+                                                        case 0b00000001:
+                                                              VoltageMax=0;
+                                                              VoltageMin=5.115;
+                                                        break;
+                                                        
+                                                        }
+                    //         Wait_For_Switch_Up(sw);
+
+*/
