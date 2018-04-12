3492f19cae04b4a2c92afda5dc3caf887cf9721f
xliu43 <36863407+xliu43@users.noreply.github.com>
2018-04-11 19:15:39 -0600

    add Lab#8

---
+++ embeded system/Lab8-PulseAccumultor/Sources/main.c
@@ -0,0 +1,110 @@
+/**************************************************************************
+*HC12 Program:	YourProg - MiniExplanation
+*Processor:		MC9S12XDP512
+*Xtal Speed:	16 MHz
+*Author:		Marc Anderson
+*Date:		  May 25, 2016                                       
+*Details:   Skeleton File with some additions
+**************************************************************************/
+
+#include <hidef.h>         	// common defines and macros
+#include <stdio.h>		    // ANSI C Standard Input/Output functions
+#include <math.h>			      // ANSI C Mathematical functions
+
+#include "derivative.h"    	// derivative-specific definitions
+
+/********************************************************************
+*		Library includes - your libraries' header files
+********************************************************************/
+//#include "IIC0_Lib.h"
+//#include "LCD_4Line_C.h" 
+//#include "Misc_Lib.h"
+//#include "LEDS_7Seg_C.h"
+#include "LEDS_7Seg_C.h"
+
+#include "Delay_C.h"
+//#include "RYG_LEDs.h"
+
+
+
+/********************************************************************
+*		Prototypes
+********************************************************************/
+
+
+
+/********************************************************************
+*		Variables
+********************************************************************/
+
+ //unsigned char Pos;
+ unsigned int iCount=0;
+ double dSpeed=0;
+ char str[21];
+
+/********************************************************************
+*		Lookups
+********************************************************************/
+
+
+
+void main(void) 	// main entry point
+{
+	_DISABLE_COP();
+
+/********************************************************************
+*		Initializations
+********************************************************************/
+          LEDS_7Seg_Init_C();
+          PACTL=0x40;  //turn the thing on 
+          lcdInit();
+          
+           Set_R_C(0,2);
+           lcdString("Wind Speed Sensor");
+  
+ 
+
+
+/********************************************************************
+*		Main Program Code
+********************************************************************/
+	
+	for (;;)		//endless program loop
+	{
+          Delay_C(1000);
+          iCount=PACN32;        
+          Top_16Out_C(HexToBCD (iCount ));
+          PACN32=0;  
+           
+          dSpeed=0.1873*iCount+4.1643; //calculate Speed 
+            if(sprintf(str,"SenSor Freq %03u HZ", iCount) >0) {
+                   Set_R_C(1,0);
+                   lcdString(str);
+          } 
+                if(sprintf(str,"Wind Speed %03.0f HZ", dSpeed) >0) {
+                   Set_R_C(2,0);
+                   lcdString(str);
+          }  
+          
+          
+                                          
+   
+    
+
+	}
+}
+/**************************************************************************
+*		Function:  Note that functions reside outside of Main - so if you call
+*              these from within Main, you must include its prototype     
+*              above
+***************************************************************************/
+
+
+
+/********************************************************************
+*		Interrupt Service Routines
+********************************************************************/
+
+
+
+/*******************************************************************/
